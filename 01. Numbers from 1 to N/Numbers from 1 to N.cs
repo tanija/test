10c29a37f4338687fc296a8b360d8f2e2ce7de8c
tanija <tanijasuccessfully@gmail.com>
2014-06-05 10:51:03 +0300

    add

---
+++ 01. Numbers from 1 to N/Numbers from 1 to N.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter number:");
+            int n = int.Parse(Console.ReadLine());
+            for (int i =1; i <= n; i++)
+            {
+                Console.Write(i +" ");
+            }
+        }
+    }
